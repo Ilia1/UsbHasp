e3800b6334939ed6a52c87bbb0d1cc19d2e74753
Ваше Имя <you@example.com>
2019-02-11 15:14:23 +0500

    build_third_party

--- build_third_party.sh
+++ build_third_party.sh
@@ -2,27 +2,42 @@
 
 VERSION_VHCI_HCD=1.15
 VERSION_LIBUSB_VHCI=0.7
-cd third_party
-#git clone https://git.code.sf.net/p/usb-vhci/vhci_hcd
+a=0
+if [ -e third_party ] 
+	then cd third_party
+	else mkdir third_party
+		cd third_party
+	fi
+if [ -e vhci-hcd-$VERSION_VHCI_HCD ]
+        then cd vhci-hcd-$VERSION_VHCI_HCD
+	else if [ -e vhci-hcd-$VERSION_VHCI_HCD.tar.gz ]
+	        then tar -xzf vhci-hcd-$VERSION_VHCI_HCD.tar.gz
+			cd vhci-hcd-$VERSION_VHCI_HCD
+		else #git clone https://git.code.sf.net/p/usb-vhci/vhci_hcd
 wget https://sourceforge.net/projects/usb-vhci/files/linux%20kernel%20module/vhci-hcd-$VERSION_VHCI_HCD.tar.gz
-if $?!=0 then echo "Can\`t get vhci-hcd kernel module. Check Internet connection"
+#if $?!=0 then echo "Cant get vhci-hcd kernel module. Check Internet connection"
 tar -xzf vhci-hcd-$VERSION_VHCI_HCD.tar.gz
 cd vhci-hcd-$VERSION_VHCI_HCD
+fi fi 
 sed -i "/#define DEBUG/d" usb-vhci-hcd.c
 sed -i "/#define DEBUG/d" usb-vhci-iocifc.c
 make
-if $?!=0 then echo "ERR:Can\`t compile vhci-hcd module"
+#if $?!=0 then echo "ERR:Cant compile vhci-hcd module"
 make install
-if $?!=0 then echo "You are sudo?"
+if [ "$?" -ne "$a" ] 
+then 
+	echo -e "\033[31mYou are sudo?" 
+else echo "OK"
+fi
 cd ..
-wget https://sourceforge.net/projects/usb-vhci/files/native%20libraries/libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
-if $?!=0 then echo "Check Internet connection"
-tar -xzf libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
+#wget https://sourceforge.net/projects/usb-vhci/files/native%20libraries/libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
+#if $?!=0 then echo "Check Internet connection"
+#tar -xzf libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
 #git clone https://git.code.sf.net/p/usb-vhci/libusb_vhci
-cd libusb_vhci-$VERSION_LIBUSB_VHCI
-./configure
-make
-if $?!=0 then echo "ERR:Can\`t compile libusb-vhci"
-make install
-if $?!=0 then echo "You are sudo?"
+#cd libusb_vhci-$VERSION_LIBUSB_VHCI
+#./configure
+#make
+#if $?!=0 then echo "ERR:Cant compile libusb-vhci"
+#make install
+#if $?!=0 then echo "You are sudo?"
 echo "Done. Add line to /etc/modules and PATH=/usr/local/lib"
