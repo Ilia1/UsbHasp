ca1f12264ca95ae8b9edddfe0db1e2f3f405ff9e
Ваше Имя <you@example.com>
2019-02-11 15:14:23 +0500

    fixing build_third_party

--- build_third_party.sh
+++ build_third_party.sh
@@ -2,27 +2,88 @@
 
 VERSION_VHCI_HCD=1.15
 VERSION_LIBUSB_VHCI=0.7
-cd third_party
-#git clone https://git.code.sf.net/p/usb-vhci/vhci_hcd
-wget https://sourceforge.net/projects/usb-vhci/files/linux%20kernel%20module/vhci-hcd-$VERSION_VHCI_HCD.tar.gz
-if $?!=0 then echo "Can\`t get vhci-hcd kernel module. Check Internet connection"
+ID=$(cat /etc/os-release | grep ^ID=  | cut -f2 -d =)
+VERSION_ID=$(cat /etc/os-release | grep ^VERSION\_ID=  | cut -f2 -d = | tr -d \")
+ID_LIKE=$(cat /etc/os-release | grep ID_LIKE | cut -f2 -d = |cut -f1 -d " " | tr -d \")
+if [ $ID_LIKE = debian ]
+then apt-get install linux-source wget make linux-headers-generic gcc libjansson4 libjansson-dev
+	apt-mark hold linux-source linux-headers-generic linux-image-generic
+elif [ $ID_LIKE = rhel ]
+then yum install -y make gcc-c++ wget jansson jansson-devel kernel-headers kernel-devel epel-release centos-release-scl
+	echo "exclude=kernel kernel-devel kernel-headers" >> /etc/yum.conf
+	yum install devtoolset-7-gcc*
+else echo "Unknown Linux distr. Install manual wget, make, gcc, jansson-dev, git, kernel headers and source and compile manual"
+	echo "Continue?"
+	read -n 1
+fi
+
+if [ -e third_party ] 
+	then cd third_party
+	else mkdir third_party
+		cd third_party
+	fi
+if [ -e vhci-hcd-$VERSION_VHCI_HCD ]
+        then cd vhci-hcd-$VERSION_VHCI_HCD
+	else if [ -e vhci-hcd-$VERSION_VHCI_HCD.tar.gz ]
+	        then tar -xzf vhci-hcd-$VERSION_VHCI_HCD.tar.gz
+			cd vhci-hcd-$VERSION_VHCI_HCD
+		else wget "https://sourceforge.net/projects/usb-vhci/files/linux kernel module/vhci-hcd-$VERSION_VHCI_HCD.tar.gz"
 tar -xzf vhci-hcd-$VERSION_VHCI_HCD.tar.gz
 cd vhci-hcd-$VERSION_VHCI_HCD
+fi fi 
 sed -i "/#define DEBUG/d" usb-vhci-hcd.c
 sed -i "/#define DEBUG/d" usb-vhci-iocifc.c
 make
-if $?!=0 then echo "ERR:Can\`t compile vhci-hcd module"
 make install
-if $?!=0 then echo "You are sudo?"
+if [ "$?" -ne 0 ] 
+then 
+	echo -e "\033[31mYou are sudo?" 
+else echo "OK"
+fi
 cd ..
-wget https://sourceforge.net/projects/usb-vhci/files/native%20libraries/libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
-if $?!=0 then echo "Check Internet connection"
+if [ -e libusb_vhci-$VERSION_LIBUSB_VHCI ]
+        then cd libusb_vhci-$VERSION_LIBUSB_VHCI
+	else if [ -e libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz ]
+	        then tar -xzf libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
+			cd libusb_vhci-$VERSION_LIBUSB_VHCI
+		else wget "https://sourceforge.net/projects/usb-vhci/files/native libraries/libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz"
 tar -xzf libusb_vhci-$VERSION_LIBUSB_VHCI.tar.gz
-#git clone https://git.code.sf.net/p/usb-vhci/libusb_vhci
 cd libusb_vhci-$VERSION_LIBUSB_VHCI
+fi fi 
 ./configure
 make
-if $?!=0 then echo "ERR:Can\`t compile libusb-vhci"
 make install
-if $?!=0 then echo "You are sudo?"
-echo "Done. Add line to /etc/modules and PATH=/usr/local/lib"
+if [ "$?" -ne 0 ] 
+then 
+	echo -e "\033[31mYou are sudo?" 
+else echo "OK"
+fi
+touch /etc/modules-load.d/usb-vhci.conf
+echo usb-vhci-hcd > /etc/modules-load.d/usb-vhci.conf
+echo usb-vhci-iocifc >> /etc/modules-load.d/usb-vhci.conf
+
+echo "Installing haspd from Etersoft"
+if [ $ID = debian ]
+then wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Debian/$VERSION_ID/haspd-modules_7.60-eter1debian_amd64.deb
+	wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Debian/$VERSION_ID/haspd_7.60-eter1debian_amd64.deb
+	dpkg -i haspd_7.60-eter1debian_amd64.deb
+	dpkg -i haspd-modules_7.60-eter1debian_amd64.deb
+elif [ $ID_LIKE = rhel ]
+then wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/RHEL/$VERSION_ID/haspd-7.60-eter1centos.x86_64.rpm
+	wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/RHEL/$VERSION_ID/haspd-modules-7.60-eter1centos.x86_64.rpm
+	yum install -y haspd-7.60-eter1centos.x86_64.rpm
+	yum install -y haspd-modules-7.60-eter1centos.x86_64.rpm
+elif [ $ID = ubuntu ]
+then wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Ubuntu/$VERSION_ID/haspd-modules_7.60-eter1ubuntu_amd64.deb || wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Ubuntu/18.04/haspd-modules_7.60-eter1ubuntu_amd64.deb
+	wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Ubuntu/$VERSION_ID/haspd_7.60-eter1ubuntu_amd64.deb || wget http://download.etersoft.ru/pub/Etersoft/HASP/last/x86_64/Ubuntu/18.04/haspd_7.60-eter1ubuntu_amd64.deb
+	dpkg -i haspd_7.60-eter1ubuntu_amd64.deb
+	dpkg -i haspd-modules_7.60-eter1ubuntu_amd64.deb
+fi
+cd ../..
+if [ $ID_LIKE = rhel ]
+then scl enable devtoolset-7 "make all"
+else make all
+fi
+echo "export PATH=\$PATH:$PWD/dist/Release/GNU-Linux " >> ~/.bash_profile
+echo "export LD_LIBRARY_PATH=/usr/local/lib" >> ~/.bash_profile
+echo "Done. Reboot and run usbhasp -d hasp.json"
